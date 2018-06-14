1a1a8cd96b85ddebfcfe085927ac88529455b044
Emma Fox <eafoxx2@gmail.com>
2018-06-14 04:10:17 +0000

    templates

--- app.py
+++ app.py
@@ -17,18 +17,26 @@ from bokeh.embed import components
 
 app = Flask(__name__)
 
-@app.route('/')
+app.vars={}
+@app.route('/',methods=['GET','POST'])
 def index():
-	in_params = {}
-	return render_template('index.html')
+	in_vars = {}
+	if request.method == 'POST':
+		app.vars['tickerCode']=request.form['ht_tickerCode']
+		app.vars['startDate']=request.form['ht_startDate']
+		app.vars['endDate']=request.form['ht_endDate']
+		
+		app.vars['valSets']=request.form.getlist('val-sets')
+		
+		tempDF = requestData(app.vars)
+		tempPlot = plotRequest(tempDF, in_vars)
+		
+		script, div = components(tempPlot)		
+		return render_template('plot.html',tempScript=script,tempDiv=div)
+		
+	else:
+		return render_template('index.html')
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
 
 #####################
 # GET DATA FROM API #
@@ -36,10 +44,10 @@ if __name__ == "__main__":
 
 def requestData(in_params):
 	apiKey="-z5FzzicL2ERpgaJrzxs" #os.environ.get('QUANDL_KEY')
-	startDate="2017-01-01" #in_params['startDate']
-	endDate="2017-12-31" #in_params['endDate']
 
-	req_params = {"api_key": apiKey, "ticker": "AAPL" , "qopts.columns": "ticker,date,open,close,adj_open,adj_close", "date.gte": startDate , "date.lte": endDate} 
+	req_params = {"api_key": apiKey, "ticker": in_params['tickerCode'], 
+	"qopts.columns": "ticker,date,open,close,adj_open,adj_close", 
+	"date.gte": in_params['startDate'], "date.lte": in_params['endDate']} 
 	data_raw = requests.get("https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json", params=req_params) #print(data_raw.status_code)
 
 
@@ -48,22 +56,42 @@ def requestData(in_params):
 	data_col=[col['name'] for col in data_table['columns']]
 	panda_table=pd.DataFrame(data_table['data'],columns=data_col)
 	panda_table['date']=pd.to_datetime(panda_table['date'], format='%Y-%m-%d')
+	
+	return(panda_table)
 
 ###################
 # PLOT WITH BOKEH #
 ###################
 def plotRequest(df, in_params):
-	source = ColumnDataSource(data=panda_table)
+	source = ColumnDataSource(data=df)
 
-	p = figure(title= "Stock Prices from Quandl WIKI", 
+	stockPlot = figure(title= "Stock Prices from Quandl WIKI", 
 	plot_height=650, plot_width=800,
 	x_axis_label='Date', x_axis_type='datetime',
 	y_axis_label='Value (in USD)',
 	tools="reset,undo,redo,pan,tap,box_zoom,box_select,hover")
 
-	p.line(x='date',y='open',source=source,legend="Opening Price",line_width=2,color="darkgreen")
-	p.line(x='date',y='close',source=source,legend="Closing Price",line_width=2,color="darkred")
-	p.line(x='date',y='adj_open',source=source,legend="Opening Price (adjusted)",line_width=2,color="limegreen")
-	p.line(x='date',y='adj_close',source=source,legend="Closing Price (adjusted)",line_width=2,color="red")
+	if 'ht_open' in app.vars['valSets'] or not app.vars['valSets']:
+		stockPlot.line(x='date',y='open',source=source,legend="Opening Price",line_width=2,color="darkgreen")
+	if 'ht_close' in app.vars['valSets'] or not app.vars['valSets']:
+		stockPlot.line(x='date',y='close',source=source,legend="Closing Price",line_width=2,color="darkred")
+	if 'ht_open-a' in app.vars['valSets'] or not app.vars['valSets']:
+		stockPlot.line(x='date',y='adj_open',source=source,legend="Opening Price (adjusted)",line_width=2,color="limegreen")
+	if 'ht_close-a' in app.vars['valSets'] or not app.vars['valSets']:
+		stockPlot.line(x='date',y='adj_close',source=source,legend="Closing Price (adjusted)",line_width=2,color="red")
+	
+	return(stockPlot)
 
+###############
+# WRAPPING UP #
+###############
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+ 
 
